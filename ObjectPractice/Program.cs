5b65214a18935c287c4714dd896bd94b11a81dd1
ESchulstad <eschulstad@gmail.com>
2016-11-09 18:21:55 -0500

    initial commit

---
+++ ObjectPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randy = new Random();
+            
+
+            for(int i = 0; i < 10; i++ )
+            {
+                int newSurprise = randy.Next(101);
+                Console.WriteLine(newSurprise + 100);
+
+            }
+        }
+    }
+}
